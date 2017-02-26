19bc4947b06464aafde8c7799d578700416dd9ed
rxon <rxon@users.noreply.github.com>
2017-02-26 21:30:16 +0900

    Add 一貫性 to install.sh
    # Conflicts:
    #	readme.md

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ url="https://raw.githubusercontent.com/rxon/dotfiles/master"
 
 for i in brew.sh node.sh cask.sh atom.sh .macos
 do
-bash -c "$(curl -fsSL $url/$i)"
+  curl -fsSL $url/$i | bash
 done
 
 curl -fsSL zplug.sh/installer | zsh
