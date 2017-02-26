3a7e749203b8e391e9143ea9f9dfd9441cc6bb13
rxon <rxon@users.noreply.github.com>
2017-02-26 21:33:04 +0900

    Add projects.sh

---
+++ projects.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Type your GitHub id : "
+read ghid
+
+repos=`curl -fsSL https://api.github.com/users/$ghid/repos | jq -r '.[].clone_url'`
+
+for i in $repos
+do
+  get $i
+done
